65bfa1eda0bad7898c8270148750412a383f1f46
Raktim-b <raktimb414@gmail.com>
2025-11-22 01:10:00 +0530

    luxy added

--- script/script.js
+++ script/script.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
+  luxy.init({
+    wrapper: "#luxy",
+    targets: ".luxy-el",
+    wrapperSpeed: 0.08,
+  });
   const menuBtn = document.querySelector(".menu-btn");
   const navMenu = document.querySelector(".nav-collapsed");
 
@@ -19,5 +24,30 @@ document.addEventListener("DOMContentLoaded", () => {
       disableOnInteraction: false,
       pauseOnMouseEnter: true,
     },
+  });
+  function countUp(el) {
+    let target = +el.getAttribute("data-target");
+    let count = 0;
+
+    let speed = target / 200; // lower = slower
+
+    let update = () => {
+      count += speed;
+      if (count < target) {
+        el.innerText = Math.floor(count);
+        requestAnimationFrame(update);
+      } else {
+        el.innerText = target + "+";
+      }
+    };
+    update();
+  }
+
+  document.querySelectorAll(".stat-number").forEach((num) => {
+    num.setAttribute(
+      "data-target",
+      num.innerText.replace("+", "").replace(",", "")
+    );
+    countUp(num);
   });
 });
